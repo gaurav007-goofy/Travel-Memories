f036e7cd81b15d84695d36fe7b65fa2cc1d43c52
spikee <thegauravsaini007@gmail.com>
2023-02-21 17:02:54 +0530

    added env

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -12,14 +14,11 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://goofy_gaurav:travelmemories007@cluster0.lqtdxob.mongodb.net/?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`server running on port:${PORT}`))
   )
